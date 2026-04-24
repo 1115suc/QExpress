2e781263e779afde0c773924b32c1bc5a820df9a
1115cr <3214774153@qq.com>
2026-04-24 14:04:00 +0800

    搭建订单微服务模块

---
+++ QExpress-ms-oms/QExpress-ms-oms-api/src/main/java/course/QExpress/oms/api/GoodFeign.java
@@ -0,0 +1,23 @@
+package course.QExpress.oms.api;
+
+import course.QExpress.oms.domain.dto.OrderCargoDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.List;
+
+@FeignClient(name = "QExpress-ms-oms", path = "good", contextId = "good")
+@ApiIgnore
+public interface GoodFeign {
+
+    /**
+     * 批量查询货物信息表
+     *
+     * @param name
+     * @return
+     */
+    @GetMapping("/hot")
+    List<OrderCargoDTO> list(@RequestParam(name = "name", required = false) String name);
+}
